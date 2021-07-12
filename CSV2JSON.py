f88f97a8e0198825a7c034335c5a9ddaaf9f8ce6
Christian Koski <64987609+ckosk@users.noreply.github.com>
2021-07-12 15:00:20 -0500

    Fixed "missing memofile" error

--- CSV2JSON.py
+++ CSV2JSON.py
@@ -1,6 +1,9 @@
 import csv
 import json
-from dbfread import DBF
+from dbfread import DBF 
+
+#NOTE In this (BDF.py) .py file you have to change "ignore_missing_memofile=False" to "ignore_missing_memofile=True"
+#NOTE (Line 85, in my case)
 
 # Function to convert a CSV to JSON
 def make_json(csvFilePath, jsonFilePath):
@@ -9,7 +12,7 @@ def make_json(csvFilePath, jsonFilePath):
         csvReader = csv.DictReader(csvf)
         #Convert each row into a dict and add it to data
         for rows in csvReader:
-            key = rows['ID'] #THIS IS THE PRIMARY KEY, PICK YOUR FIRST COLUMN
+            key = rows['PRTYPE_IDL'] #THIS IS THE PRIMARY KEY, PICK YOUR FIRST COLUMN
             data[key] = rows
     #Open json writer and dump data
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
@@ -25,8 +28,8 @@ def dbf_to_csv(dbf_table_pth):#Input a dbf, outputs a csv, same name and same pa
             writer.writerow(list(record.values()))
     return csv_fn #returns a csv file name, CAN be used in the make_json function
          
-csvFilePath = r'Products.csv'
-jsonFilePath = r'HereYouGo.json'
-dbfFilePath = r'Products.dbf'
+csvFilePath = r'example.csv' #IF CONVERTING FROM CSV this is the name of the csv file you want to convert, otherwise, not needed
+jsonFilePath = r'finished.json' #NOTE This is where you pick the name of the JSON file that will be output
+dbfFilePath = r'project.dbf' #NOTE This is the name of the DBF file you want to convert to JSON
 
-make_json(csvFilePath, jsonFilePath) #INSTEAD OF "csvFilePath" PUT "dbf_to_csv(dbfFilePath)" IF TRYING TO CONVERT DBF
+make_json(dbf_to_csv(dbfFilePath), jsonFilePath) #INSTEAD OF "csvFilePath" PUT "dbf_to_csv(dbfFilePath)" IF TRYING TO CONVERT DBF
