9f383733cc47dd127c38d47c1ccd4a17f83e7105
Christian Koski <64987609+ckosk@users.noreply.github.com>
2021-07-12 13:51:30 -0500

    small edits/comments

--- CSV2JSON.py
+++ CSV2JSON.py
@@ -7,29 +7,26 @@ def make_json(csvFilePath, jsonFilePath):
     data = {}
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
-        # Convert each row into a dictionary
-        # and add it to data
+        #Convert each row into a dict and add it to data
         for rows in csvReader:
-            # Assuming a column named 'ID' to
-            # be the primary key
-            key = rows['PROD_IDL'] #THIS IS THE PRIMARY KEY
+            key = rows['ID'] #THIS IS THE PRIMARY KEY, PICK YOUR FIRST COLUMN
             data[key] = rows
-    # Open a json writer and dump data
+    #Open json writer and dump data
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
 
 def dbf_to_csv(dbf_table_pth):#Input a dbf, outputs a csv, same name and same path
     csv_fn = dbf_table_pth[:-4]+ ".csv" #Set csv file name
     table = DBF(dbf_table_pth)
-    with open(csv_fn, 'w', newline = '') as f:# create a csv file and fill with dbf content
+    with open(csv_fn, 'w', newline = '') as f:#Create a csv file and fill with dbf content
         writer = csv.writer(f)
-        writer.writerow(table.field_names)# write column name
-        for record in table:# write rows
+        writer.writerow(table.field_names)#Write column name
+        for record in table:#Write rows
             writer.writerow(list(record.values()))
-    return csv_fn
+    return csv_fn #returns a csv file name, CAN be used in the make_json function
          
 csvFilePath = r'Products.csv'
-jsonFilePath = r'Products.json'
+jsonFilePath = r'HereYouGo.json'
 dbfFilePath = r'Products.dbf'
 
-make_json(dbf_to_csv(dbfFilePath), jsonFilePath)
+make_json(csvFilePath, jsonFilePath) #INSTEAD OF "csvFilePath" PUT "dbf_to_csv(dbfFilePath)" IF TRYING TO CONVERT DBF
